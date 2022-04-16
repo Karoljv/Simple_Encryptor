18cb04c09512e251bb565df9f14fd6c5015995d6
Karoljv <93725497+Karoljv@users.noreply.github.com>
2022-04-16 15:04:07 +0200

    Update table_conversion.py

--- table_conversion.py
+++ table_conversion.py
@@ -1,18 +1,22 @@
 from random import randrange
-
+from json import dump
 class TableConversion:
     alfabet = "qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM"
-    def create_key()->str:
-        
+    def create_key()->str:  
         code = []
-
+        dic = {}
         for i in range(len(TableConversion.alfabet)):
             while True: 
                 index = randrange(0,len(TableConversion.alfabet))
-                if index == i or TableConversion.alfabet[index] in code:
+                if TableConversion.alfabet[index] in code:
                     continue
                 else:
                     break
+            
+            dic[TableConversion.alfabet[i]] = TableConversion.alfabet[index]
             code.append(TableConversion.alfabet[index])
         
+        with open("conversion_table.json","w") as json_file:
+            dump(dic,json_file,indent=4)
+
         return ''.join(code)
